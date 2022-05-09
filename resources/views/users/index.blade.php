4513123287ecf65632521775adb68ad798a4cd54
Andrés Perdomo <alvarado0925@gmail.com>
2022-05-09 12:25:57 -0500

    🧪Feat (users): Add validations from create

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,19 +7,29 @@
             <div class="card-body">
                 <form action="{{ route('users.store') }}" method="post" class="row gy-2 gx-3 align-items-center" autocomplete="off">
                     <div class="col-auto">
-                        <input type="text" name="name" class="form-control form-control-sm" placeholder="Nombre" required>
+                        <input type="text" name="name" class="form-control form-control-sm" placeholder="Nombre" value="{{ @old('name') }}">
                     </div>
                     <div class="col-auto">
-                        <input type="email" name="email" class="form-control form-control-sm" placeholder="Email" required>
+                        <input type="email" name="email" class="form-control form-control-sm" placeholder="Email" value="{{ @old('email') }}">
                     </div>
                     <div class="col-auto">
-                        <input type="password" name="password" class="form-control form-control-sm" placeholder="Contraseña" required>
+                        <input type="password" name="password" class="form-control form-control-sm" placeholder="Contraseña" value="{{ @old('password') }}">
                     </div>
                     <div class="col-auto">
                         @csrf
                         <button type="submit" class="btn btn-sm btn-success">Enviar</button>
                     </div>
                 </form>
+                @if ($errors->any())
+                    <div class="alert alert-danger alert-dismissible fade show mt-3 mb-0" role="alert">
+                        <ul class="mb-0">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
             </div>
         </div>
 
