f4f696a9575a6ccd7577157b55da6aec05e3f061
Andrés Perdomo <alvarado0925@gmail.com>
2022-05-12 11:45:57 -0500

    🧪Feat: Do examples with collections and serializations

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 use App\Post;
+use App\User;
 
 use function PHPUnit\Framework\callback;
 
@@ -27,3 +28,20 @@ Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
 Route::view('posts', 'posts', [
     'posts' => Post::where('id', '<=', 10)->get()
 ])->name('posts')->middleware('auth');
+
+Route::get('collections', function () {
+    $users = User::all();
+    //dd($users);
+    //dd($users->contains(7));// Verifica si existe un id
+    //dd($users->except([1, 2]));
+    //dd($users->only(5));
+    //dd($users->find(5));// A diferencia de only, esta accede inmediatamente a los datos de ese objeto
+    dd($users->load('posts'));// Carga la relación 'posts' a la colección retornada
+});
+
+Route::get('serializations', function () {
+    $users = User::all();
+    //dd($users->toArray());// Convierte la colección en un array en vez de un objeto
+    //dd($users->toJson());// Convierte la colección en un objeto json
+    dd($users->find(4)->toJson());
+});
